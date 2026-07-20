9fdb94b4d2bcb55146ef65bde4f2012e90fff968
Telman Asgarov <telman99787@gmail.com>
2026-07-20 11:49:36 +0400

    Add User repository

---
+++ src/main/java/com/qwasar/todolist/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.qwasar.todolist.repository;
+
+import com.qwasar.todolist.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existByUsername(String username);
+
+    boolean existByEmail(String email);
+
+}
