b6ec88520dd39af90be8b2548325cf5bc30fb163
Telman Asgarov <telman99787@gmail.com>
2026-07-22 16:08:20 +0400

    Added user details service for basic auth

---
+++ src/main/java/com/qwasar/todolist/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.qwasar.todolist.security;
+
+import com.qwasar.todolist.entity.User;
+import com.qwasar.todolist.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(
+            UserRepository userRepository
+    ) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(
+                                "User not found: " + username
+                        )
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
