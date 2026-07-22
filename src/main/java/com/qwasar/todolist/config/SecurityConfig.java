3dd8dc82cb2345d6f79a355d5fdfe90e82bbefcb
Telman Asgarov <telman99787@gmail.com>
2026-07-22 16:11:58 +0400

    Added security config

---
+++ src/main/java/com/qwasar/todolist/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.qwasar.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/api/auth/register",
+                                "/api/auth/login"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
